5603c30d91aac7a355d16f6e48d939797797c901
Michael Pavlak <mike@stashstock.com>
2020-05-15 21:26:28 -0400

    optimize parser to read stream instead of buffer json string when json token selector is not specified.  do not parse response body just to determine if the response was successful.

--- CaptainPav.WebApiProxy/DefaultHttpResponseMessageReader.cs
+++ CaptainPav.WebApiProxy/DefaultHttpResponseMessageReader.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -31,6 +32,42 @@ namespace CaptainPav.WebApiProxy
 			return ReadMessageIfSuccessfulOrThrowAsync(message, this.ExceptionReconstructor, validCodes);
 		}
 
+		public Task<Stream> GetStreamIfSuccessfulOrThrowAsync(HttpResponseMessage message, params HttpStatusCode[] validCodes)
+		{
+			return GetStreamIfSuccessfulOrThrowAsync(message, this.ExceptionReconstructor, validCodes);
+		}
+
+		public Task ThrowIfNotSuccessfulAsync(HttpResponseMessage message, params HttpStatusCode[] validCodes)
+		{
+			return ThrowIfNotSuccessfulAsync(message, this.ExceptionReconstructor, validCodes);
+		}
+
+		/// <summary>
+		/// Throws an exception if the response message indicates
+		/// an unsuccessful result
+		/// </summary>
+		/// <param name="message">
+		/// The <see cref="HttpResponseMessage"/> to check for failure
+		/// </param>
+		/// <param name="reconstructor">
+		/// When the <see cref="HttpResponseMessage"/> indicates an unsuccessful
+		/// response, then this reconstructor is used to try and reconstruct the
+		/// original exception.
+		/// </param>
+		/// <param name="validCodes">
+		/// The set of <see cref="HttpStatusCode"/>s which represent a successful call
+		/// </param>
+		/// <exception cref="UnexpectedStatusCodeException">
+		/// Thrown if the <see cref="HttpResponseMessage"/> indicates an unsuccessful status code
+		/// </exception>
+		public static async Task ThrowIfNotSuccessfulAsync(HttpResponseMessage message, IExceptionReconstructor reconstructor, params HttpStatusCode[] validCodes)
+		{
+			if (!message.IsResponseSuccessful(validCodes))
+			{
+				throw message.CreateExceptionIfNotSuccessful(await reconstructor.Reconstruct(message).ConfigureAwait(false), validCodes);
+			}
+		}
+
 		/// <summary>
 		/// Attempts to read the <see cref="HttpResponseMessage"/> body as a
 		/// string and throws an exception if the response message indicates
@@ -40,7 +77,7 @@ namespace CaptainPav.WebApiProxy
 		/// The <see cref="HttpResponseMessage"/> to read
 		/// </param>
 		/// <param name="reconstructor">
-		///  When the <see cref="HttpResponseMessage"/> indicates an unsuccessful
+		/// When the <see cref="HttpResponseMessage"/> indicates an unsuccessful
 		/// response, then this reconstructor is used to try and reconstruct the
 		/// original exception.
 		/// </param>
@@ -52,16 +89,43 @@ namespace CaptainPav.WebApiProxy
 		/// otherwise, an exception is thrown.
 		/// </returns>
 		/// <exception cref="UnexpectedStatusCodeException">
-		/// Throw if the <see cref="HttpResponseMessage"/> indicates an unsuccessful status code
+		/// Thrown if the <see cref="HttpResponseMessage"/> indicates an unsuccessful status code
 		/// </exception>
 		public static async Task<string> ReadMessageIfSuccessfulOrThrowAsync(HttpResponseMessage message, IExceptionReconstructor reconstructor, params HttpStatusCode[] validCodes)
 		{
-			if (!message.IsResponseSuccessful(validCodes))
-			{
-				throw message.CreateExceptionIfNotSuccessful(await reconstructor.Reconstruct(message).ConfigureAwait(false), validCodes);
-			}
+			await ThrowIfNotSuccessfulAsync(message, reconstructor, validCodes).ConfigureAwait(false);
 
 			return await message.Content.ReadAsStringAsync().ConfigureAwait(false);
+		}
+
+		/// <summary>
+		/// Attempts to read the <see cref="HttpResponseMessage"/> body stream
+		/// and throws an exception if the response message indicates
+		/// an unsuccessful result
+		/// </summary>
+		/// <param name="message">
+		/// The <see cref="HttpResponseMessage"/> to read
+		/// </param>
+		/// <param name="reconstructor">
+		/// When the <see cref="HttpResponseMessage"/> indicates an unsuccessful
+		/// response, then this reconstructor is used to try and reconstruct the
+		/// original exception.
+		/// </param>
+		/// <param name="validCodes">
+		/// The set of <see cref="HttpStatusCode"/>s which represent a successful call
+		/// </param>
+		/// <returns>
+		/// The content body stream if the request indicates success;
+		/// otherwise, an exception is thrown.
+		/// </returns>
+		/// <exception cref="UnexpectedStatusCodeException">
+		/// Thrown if the <see cref="HttpResponseMessage"/> indicates an unsuccessful status code
+		/// </exception>
+		public static async Task<Stream> GetStreamIfSuccessfulOrThrowAsync(HttpResponseMessage message, IExceptionReconstructor reconstructor, params HttpStatusCode[] validCodes)
+		{
+			await ThrowIfNotSuccessfulAsync(message, reconstructor, validCodes).ConfigureAwait(false);
+
+			return await message.Content.ReadAsStreamAsync().ConfigureAwait(false);
 		}
 	}
 }
